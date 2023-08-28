a6ca5a708b8b3b9b9196d436dc9338ee15b25630
柠檬冲水 <916415899@qq.com>
2023-08-28 19:49:49 +0800

    升级脚手架

--- bin/index.ts
+++ bin/index.ts
@@ -2,7 +2,7 @@
 
 import prompts from "prompts";
 import { existsSync, mkdirSync } from "fs";
-import { copy } from "fs-extra";
+import fse from "fs-extra";
 import { spawn } from "child_process";
 import { resolve, join, dirname } from "path";
 import { fileURLToPath } from "node:url";
@@ -33,50 +33,95 @@ async function runNpmCommand(command, args) {
   });
 }
 
-prompts([
-  {
-    type: "text",
-    name: "name",
-    message: "robot name:",
-    validate: async (value: any) => {
-      if (existsSync(`./${value}`)) return "Robot name already exists!";
-      return /^[a-z][0-9a-z_-]{0,}$/.test(value)
-        ? true
-        : "首字母小写,可选符号数字、小写字母、_和-";
-    },
-    initial: process.argv[3] ? process.argv[3] : "alemon-bot",
-  },
-])
-  .then(async ({ name }) => {
-    // 强制退出错误
-    if (!name) process.exit();
-    const dirPath = `./${name}`;
-    mkdirSync(dirPath);
-    console.log(`\n`);
+const args = process.argv;
 
-    const currentFilePath = fileURLToPath(import.meta.url);
-    const currentDirPath = dirname(currentFilePath);
-    try {
-      const alemonCliPath = resolve(currentDirPath);
-      const templatePath = join(alemonCliPath, "template");
-      console.log("Copying template...");
-      await copy(templatePath, dirPath);
-      process.chdir(dirPath);
-      console.log("Loading dependencies...");
-      await runNpmCommand("npm", ["install"]);
-      console.log("Alemon-Bot cloned successfully!");
-      console.log(`------------------------------------`);
-      console.log(`cd ${name}      #Entering`);
-      console.log(`npm run app:qq     #启动qq频道机器人`);
-      console.log(`npm run app:dc     #启动discord机器人`);
-      console.log(`npm run app:mys    #启动villa机器人`);
-      console.log(`npm run app:kook    #启动KOOK机器人`);
-    } catch (error) {
-      console.log(`${name} ${error}`);
+async function createAlemon({ name, force = false }) {
+  // 错误参数
+  if (!name) process.exit();
+  // 锁定位置
+  const dirPath = `./${name}`;
+
+  // 如果f为真,不敢存不存在,直接
+
+  // 已经存在目录
+  if (existsSync(dirPath)) {
+    if (!force) {
+      console.log("Robot name already exists!");
       return;
     }
-  })
-  .catch((err) => {
-    console.log(err);
+    await fse.removeSync(dirPath); // 强制删除已存在的文件或目录
+  }
+
+  // 没有存在
+  mkdirSync(dirPath);
+  console.log("\n");
+  const currentFilePath = fileURLToPath(import.meta.url);
+  const currentDirPath = dirname(currentFilePath);
+  try {
+    const alemonCliPath = resolve(currentDirPath);
+    const templatePath = join(alemonCliPath, "template");
+    console.log("Copying template...");
+    await fse.copy(templatePath, dirPath);
+    process.chdir(dirPath);
+    console.log("Loading dependencies...");
+    await runNpmCommand("npm", ["install"]);
+    console.log(`------------------------------------`);
+    console.log("Alemon-Bot cloned successfully!");
+    console.log(`------------------------------------`);
+    console.log(`cd ${name}      #进入机器人目录`);
+    console.log(`npm run app:qq     #启动qq频道机器人`);
+    console.log(`npm run app:dc     #启动discord机器人`);
+    console.log(`npm run app:mys    #启动villa机器人`);
+    console.log(`npm run app:kook    #启动KOOK机器人`);
+  } catch (error) {
+    console.log(`${name} ${error}`);
     return;
-  });
+  }
+}
+
+if (!args[2]) {
+  createAlemon({ name: "alemon-bot" });
+}
+
+if (args[2]) {
+  const BotName = args[2];
+  if (!args[3]) {
+    createAlemon({ name: BotName });
+  }
+  if (args[3]) {
+    switch (args[3]) {
+      // 强制覆盖  不管存不存在,直接覆盖
+      case "f": {
+        createAlemon({ name: BotName, force: true });
+        break;
+      }
+      // 想重输入
+      case "d": {
+        prompts([
+          {
+            type: "text",
+            name: "name",
+            message: "robot name:",
+            validate: async (value: any) => {
+              if (existsSync(`./${value}`)) return "Robot name already exists!";
+              return /^[a-z][0-9a-z_-]{0,}$/.test(value)
+                ? true
+                : "首字母小写,可选符号数字、小写字母、_和-";
+            },
+            initial: "alemon-bot",
+          },
+        ])
+          .then(createAlemon)
+          .catch((err) => {
+            console.log(err);
+            return;
+          });
+        break;
+      }
+      default: {
+        console.log("无效参数~");
+        break;
+      }
+    }
+  }
+}
