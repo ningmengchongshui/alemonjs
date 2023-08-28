28a1fdf9b89bac9c8f85f0a481bcb48035896b7c
柠檬冲水 <916415899@qq.com>
2023-08-28 16:27:41 +0800

    新模板

--- commands/template/plugins/alemon-plugin/db/redis/index.ts
+++ commands/template/plugins/alemon-plugin/db/redis/index.ts
@@ -1,8 +1,8 @@
-import { createRedis } from 'alemon-redis'
-// 不传入配置地址,默认读取config/redis.yaml
-export const redisA = createRedis()
+import { createRedis, Redis as RedisClient } from 'alemon-redis'
+// 不传入配置则使用默认参数
+export const redisA: RedisClient = createRedis()
 // 传入参数
-export const redisC = createRedis({
+export const redisC: RedisClient = createRedis({
   host: '127.0.0.1',
   port: 6379,
   password: '',
--- commands/template/plugins/alemon-plugin/devimg/defset/defset.ts
+++
@@ -1,6 +0,0 @@
-import { readFileSync } from 'node:fs'
-import { DirPath } from '../../app.config'
-export function getConfig(name) {
-  /* 读取配置 */
-  return JSON.parse(readFileSync(`${DirPath}/resources/defset/${name}.json`, 'utf8'))
-}
