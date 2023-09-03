29bf165624e7e8659d9106f23e9fa7939a979692
柠檬冲水 <916415899@qq.com>
2023-09-03 10:13:55 +0800

    更改

--- bin/template/.puppeteerrc.cjs
+++ bin/template/.puppeteerrc.cjs
@@ -3,84 +3,81 @@ const { existsSync } = require('fs')
 const { execSync } = require('child_process')
 const arch = os.arch()
 /**
- * 是否跳过下载
+ * Downloa
  */
 let skipDownload = false
 /**
- * 浏览器路径
+ * Path
  */
 let executablePath
 
 /**
- * windows浏览器路径
+ * windows path
  */
 const win32Edge = 'C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe'
 /**
  * linux | android
  */
 if (process.platform == 'linux' || process.platform == 'android') {
+  const chromium = [
+    'whereis chrome-browser',
+    'whereis chrome',
+    'whereis chromium-browser',
+    'whereis chromium',
+    'whereis firefox'
+  ]
+
   /**
-   * 如果arm64架构跳过下载
+   * get path
+   */
+  for (const item of chromium) {
+    try {
+      const chromiumPath = execSync(item).toString().split(' ')[1].trim()
+      if (chromiumPath) {
+        skipDownload = true
+        console.log(chromiumPath, '111111')
+        executablePath = realpathSync(chromiumPath)
+        console.info('[Chromium] start')
+        break
+      }
+    } catch (error) {
+      console.error('Failed to get Chromium path:', error)
+      continue
+    }
+  }
+
+  /**
+   * not path
+   */
+  if (!skipDownload) {
+    /**
+     * search
+     */
+    const arr = [
+      '/usr/bin/chromium',
+      '/snap/bin/chromium',
+      '/usr/bin/chromium-browser'
+    ]
+    for (const item of arr) {
+      if (existsSync(item)) {
+        skipDownload = true
+        executablePath = item
+        console.info('[Chromium] start')
+        break
+      }
+    }
+  }
+
+  /**
+   * arm64/arrch64
    */
   if (arch == 'arm64' || arch == 'aarch64') {
     console.info('[arm64/aarch64] system')
     skipDownload = true
   }
-
-  const map = {
-    0: 'whereis chromium-browser',
-    1: 'whereis firefox'
-  }
-
-  /**
-   * 获取路径
-   */
-  try {
-    const chromiumPath = execSync(map[0]).toString().split(' ')[1]
-    if (chromiumPath) {
-      skipDownload = true
-      executablePath = chromiumPath
-      console.info('[Chromium] start')
-      return
-    }
-  } catch (error) {
-    console.error('Failed to get Chromium path:', error)
-  }
-
-  /**
-   * 不存在 chromium-browser
-   * 但存在 whereis firefox
-   */
-  try {
-    const firefoxPath = execSync(map[1]).toString().split(' ')[1]
-    if (firefoxPath) {
-      skipDownload = true
-      executablePath = firefoxPath
-      console.info('[Firefox] start')
-      return
-    }
-  } catch (error) {
-    console.error('Failed to get Firefox path:', error)
-  }
-  /**
-   * 直接找路径
-   */
-  const arr = [
-    '/usr/bin/chromium',
-    '/snap/bin/chromium',
-    '/usr/bin/chromium-browser'
-  ]
-  for (const item of arr) {
-    if (existsSync(item)) {
-      skipDownload = true
-      executablePath = item
-      console.info('[Chromium] start')
-      break
-    }
-  }
 } else if (process.platform == 'win32' && existsSync(win32Edge)) {
   /**
-   * win32 且存在 Edge
+   * win32  Edge
    */
   skipDownload = true
   executablePath = win32Edge
