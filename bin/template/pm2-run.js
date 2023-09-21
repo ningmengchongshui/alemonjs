0e8921debff2741a490e4b156537450627b73caa
柠檬冲水 <916415899@qq.com>
2023-09-21 22:59:59 +0800

    修复后台启动

--- bin/template/pm2-run.js
+++ bin/template/pm2-run.js
@@ -2,7 +2,7 @@ import { spawn } from 'child_process'
 
 const ars = process.argv.slice(2)
 
-const command = spawn(`ts-node index.ts ${ars.join(' ')}`, { shell: true })
+const command = spawn(`ts-node src/main.ts ${ars.join(' ')}`, { shell: true })
 
 command.stdout.on('data', data => {
   process.stdout.write(data.toString())
