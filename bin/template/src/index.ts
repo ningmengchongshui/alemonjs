0943a0cec7874cdac5acd13adccce8615d39baa4
柠檬冲水 <916415899@qq.com>
2023-09-28 22:30:18 +0800

    更改版本

--- bin/template/src/index.ts
+++ bin/template/src/index.ts
@@ -1,6 +1,8 @@
-// 插件模式启动code--替换main.ts后仅加载plugins
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+if (process.argv.slice(2).includes('discord') && !process.argv.slice(2).includes('not')) {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+}
 import { createBot } from 'alemon-bot'
+import { createApp, compilationTools } from 'alemon'
 
 // 自动加载ffmpeg
 // import ffmpeg from 'alemon-ffmpeg'
@@ -8,7 +10,24 @@ import { createBot } from 'alemon-bot'
 
 // 创建机器人
 await createBot(process.argv.slice(2))
-  .then(alemon => alemon())
-  .catch(err => {
-    console.log('出错啦', err)
+  .then(alemon => alemon(false))
+  .catch((err: any) => {
+    console.log('AlemonBot:', err)
   })
+
+// 加载模块
+const alemon = await compilationTools({
+  // 对example下的所有ts文件进行编译
+  input: `example/**/*.ts`,
+  // 模块文件
+  file: `example.js`,
+  // 忽视提示
+  external: ['alemon']
+})
+
+// 创建应用
+const app = createApp('alemon')
+// 设置模块
+app.component(alemon)
+// 挂载
+app.mount('#app')
