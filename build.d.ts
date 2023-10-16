7f50862e3e2a9ab059beb528b9c6f78ed565819c
柠檬冲水 <916415899@qq.com>
2023-10-16 20:17:08 +0800

    更新

---
+++ build.d.ts
@@ -0,0 +1,1 @@
+export declare function commandBuild(): Promise<void>
--- src/build.ts
+++ src/build.ts
@@ -45,10 +45,15 @@ function createRollupCoinfg(aInput: string, aOutput: string) {
  * @param Options
  * @returns
  */
-export async function compilationTools(Options: {
-  aInput: string
-  aOutput: string
-}) {
+export async function compilationTools(
+  Options: {
+    aInput: string
+    aOutput: string
+  } = {
+    aInput: 'apps/**/*.ts',
+    aOutput: 'alemon.app.js'
+  }
+) {
   try {
     const config: RollupOptions = createRollupCoinfg(
       Options?.aInput,
@@ -78,14 +83,60 @@ export async function compilationTools(Options: {
 }
 
 /**
+ * 编译并返回应用导出
+ * @param AppName
+ * @param dir
+ * @param url
+ * @param name
+ * @returns
+ */
+export async function buildTools(
+  AppName: string,
+  dir: string,
+  url = 'apps',
+  name = 'alemon.app.js'
+) {
+  const aInput = `${dir.replace(/^\//, '')}/${AppName}/${url}/**/*.ts`
+  const aOutput = `${dir.replace(/^\//, '')}/${AppName}/${name}`
+  try {
+    const config: RollupOptions = createRollupCoinfg(aInput, aOutput)
+    // 使用 Rollup API 编译代码
+    const bundle = await rollup(config)
+    // 判断
+    if (config.output[0] && aOutput) {
+      // 写入
+      await bundle.write(config.output[0])
+      // 集成
+      const apps = await import(`file://${join(process.cwd(), aOutput)}`).catch(
+        err => {
+          console.error(err)
+          return {}
+        }
+      )
+      return apps
+    }
+    return {}
+  } catch (error) {
+    console.error(error)
+    console.error('err:', aInput)
+    return {}
+  }
+}
+
+/**
  * 编译应用
  * @param Options
  * @returns
  */
-export async function buildModulsApps(Options: {
-  aInput: string
-  aOutput: string
-}) {
+export async function buildModulsApps(
+  Options: {
+    aInput: string
+    aOutput: string
+  } = {
+    aInput: 'apps/**/*.ts',
+    aOutput: 'alemon.app.js'
+  }
+) {
   try {
     const config: RollupOptions = createRollupCoinfg(
       Options?.aInput,
