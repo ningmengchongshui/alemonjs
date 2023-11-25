a2866890c2c4724ea82cb72e09ba90df5de1551f
柠檬冲水 <916415899@qq.com>
2023-11-26 01:26:33 +0800

    更新

--- bin/afloat.ts
+++ bin/afloat.ts
@@ -1,15 +1,27 @@
 #!/usr/bin/env node
 
 import { buildModulsApps } from '../lib/index.js'
-
 import { spawn } from 'child_process'
-import { existsSync } from 'fs'
-import { join } from 'path'
+import { mkdirSync, copyFileSync, existsSync, writeFileSync } from 'fs'
+import { join, dirname } from 'path'
 
 const argv = [...process.argv]
 const arg = argv.splice(2)
+const cwd = process.cwd()
 
-if (arg.includes('build')) {
+function createFile(tar: string, dist: string) {
+  const Pkg = join(cwd, tar)
+  if (!existsSync(Pkg)) return
+  const DistPkg = join(cwd, dist)
+  if (!existsSync(DistPkg)) {
+    mkdirSync(dirname(DistPkg), { recursive: true })
+    console.log('no file')
+    writeFileSync(DistPkg, '')
+  }
+  copyFileSync(Pkg, DistPkg)
+}
+
+async function build() {
   // 看看有无参数
   const input =
     arg.indexOf('--input') != -1
@@ -23,11 +35,28 @@ if (arg.includes('build')) {
       : arg.indexOf('--o') != -1
       ? arg.indexOf('--o')
       : false
+  const put = output != false ? arg[output + 1] : 'alemon.app.js'
   await buildModulsApps({
     input: input != false ? arg[input + 1] : 'apps/**/*.{ts,js}',
-    output: output != false ? arg[output + 1] : 'alemon.app.js'
+    output: put
   })
-} else if (arg.includes('dev')) {
+  const dist =
+    arg.indexOf('--d') != -1
+      ? arg.indexOf('--d')
+      : arg.indexOf('--dir') != -1
+      ? arg.indexOf('--dir')
+      : arg.indexOf('--directory') != -1
+      ? arg.indexOf('--directory')
+      : arg.indexOf('--dist') != -1
+      ? arg.indexOf('--dist')
+      : false
+  const dir = dist != false ? arg[dist + 1] : 'dist'
+  createFile(put, `${dir}/${put}`)
+  createFile('main.js', `${dir}/main.js`)
+  createFile('package.json', `${dir}/package.json`)
+}
+
+function dev() {
   const msg = arg.join(' ')
   const files = msg.match(/(\S+\.js|\S+\.ts)/g) ?? ['afloat.config.ts']
   const argsWithoutFiles = msg.replace(/(\S+\.js|\S+\.ts)/g, '')
@@ -39,7 +68,6 @@ if (arg.includes('build')) {
       const isTypeScript = app.endsWith('.ts')
       const command = isTypeScript ? 'ts-node' : 'node'
       const cmd = `${command} ${app} ${argsWithoutFiles}`
-      console.info(cmd)
       const childProcess = spawn(cmd, { shell: true })
       childProcess.stdout.on('data', data => {
         process.stdout.write(data.toString())
@@ -49,4 +77,13 @@ if (arg.includes('build')) {
       })
     }
   }
+}
+
+/**
+ *
+ */
+if (arg.includes('dev')) {
+  dev()
+} else if (arg.includes('build')) {
+  await build()
 }
