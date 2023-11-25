3690bdd07836e73234eaaa835e41d7c946855420
柠檬冲水 <916415899@qq.com>
2023-11-25 16:32:45 +0800

    更新

--- src/define.ts
+++ src/define.ts
@@ -2,6 +2,9 @@ import nodemon, { Settings } from 'nodemon'
 import { buildModulsApps } from './index.js'
 import { commandRun } from './run.js'
 import { Anodemon } from './nodemon.js'
+import { ChildProcessWithoutNullStreams } from 'child_process'
+import treeKill from 'tree-kill'
+let app: ChildProcessWithoutNullStreams
 export function defineAfloat(options: {
   build?: {
     input: string
@@ -28,24 +31,28 @@ export function defineAfloat(options: {
       Anodemon.ignore.push(options.build.output)
     }
   }
-  const call = async () => {
-    if (options?.build?.input && options.build?.output) {
-      await buildModulsApps(options?.build)
-    }
-    commandRun(argv)
-  }
   console.log('Anodemon', Anodemon)
   // 配置 nodemon
   nodemon(Anodemon)
   // 监听启动事件
-  nodemon.on('start', () => {
+  nodemon.on('start', async () => {
     console.log('[Afloat] start')
-    call()
+    // 打包
+    if (options?.build?.input && options.build?.output) {
+      await buildModulsApps(options?.build)
+    }
+    // 取得子进程信息
+    const run = commandRun(argv)
+    if (run) app = run
   })
   // 监听文件更改事件
   nodemon.on('restart', message => {
     console.log('[Afloat] restart')
     console.log(message)
+    // 杀死前一个子进程
+    treeKill(app.pid, (error?: Error) => {
+      if (error) nodemon.emit('quit')
+    })
   })
   // 监听退出事件
   nodemon.on('quit', () => {
--- src/run.ts
+++ src/run.ts
@@ -9,21 +9,21 @@ export function commandRun(ars: string[]) {
   const msg = ars.join(' ')
   const files = msg.match(/(\S+\.js|\S+\.ts)/g) ?? ['alemon.config.ts']
   const argsWithoutFiles = msg.replace(/(\S+\.js|\S+\.ts)/g, '')
-  for (const item of files) {
-    if (!existsSync(join(process.cwd(), item))) {
-      console.info('no file', item)
-      continue
-    }
-    const isTypeScript = item.endsWith('.ts')
-    const command = isTypeScript ? 'ts-node' : 'node'
-    const cmd = `${command} ${item} ${argsWithoutFiles}`
-    console.info(cmd)
-    const childProcess = spawn(cmd, { shell: true })
-    childProcess.stdout.on('data', data => {
-      process.stdout.write(data.toString())
-    })
-    childProcess.stderr.on('data', data => {
-      process.stderr.write(data.toString())
-    })
+  const app = files[0]
+  if (!app) return false
+  if (!existsSync(join(process.cwd(), app))) {
+    console.info('no file', app)
+    return false
   }
+  const command = app.endsWith('.ts') ? 'ts-node' : 'node'
+  const cmd = `${command} ${app} ${argsWithoutFiles}`
+  console.info(cmd)
+  const childProcess = spawn(cmd, { shell: true })
+  childProcess.stdout.on('data', data => {
+    process.stdout.write(data.toString())
+  })
+  childProcess.stderr.on('data', data => {
+    process.stderr.write(data.toString())
+  })
+  return childProcess
 }
