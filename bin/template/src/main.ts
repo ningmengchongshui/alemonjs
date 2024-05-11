083e3b7e1ebc4717c685b245b150eaed25d43685
ningmengchongshui <916415899@qq.com>
2024-05-11 21:12:56 +0800

    fix: 修改版本

--- bin/template/src/main.ts
+++ bin/template/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'alemonjs'
 import * as rules from './rules'
-// import api from './api/router'
 const app = createApp(import.meta.url)
-// app.use(api)
-app.use(rules).mount()
+for (const key in rules) {
+  app.use(rules[key])
+}
+app.mount()
